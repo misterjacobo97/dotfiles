73e11a01b7567a78ae9319a64f08a7750b6ebe39
jacobo <mister.jacobo97@gmail.com>
2024-02-20 09:51:22 +1100

    initial commit for dotfile backup

---
+++ dot_config/nvim/lua/custom/plugins/nvim_config.lua
@@ -0,0 +1,8 @@
+-- guide for nvim lua - https://github.com/nanotee/nvim-lua-guide
+-- for relative line numbers
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "number"
+
+return {}
