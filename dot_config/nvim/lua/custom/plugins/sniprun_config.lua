4b53081cf6fc5032995250e0dadb14b40746436b
jacobo <mister.jacobo97@gmail.com>
2024-04-12 10:32:58 +1000

    Remove .dot_bashrc.swo Remove .dot_bashrc.swp Update .config/nvim/lua/custom/plugins/sniprun_config.lua

--- dot_config/nvim/lua/custom/plugins/sniprun_config.lua
+++ dot_config/nvim/lua/custom/plugins/sniprun_config.lua
@@ -2,11 +2,15 @@ return {
 	"michaelb/sniprun",
 	branch = "master",
 
-	build = "sh install.sh",
+	build = "sh install.sh 1",
 	-- do 'sh install.sh 1' if you want to force compile locally
 	-- (instead of fetching a binary from the github release). Requires Rust >= 1.65
 
 	config = function()
-		require("sniprun").setup({})
+		require("sniprun").setup({
+			display = { "VirtualText", "terminal" },
+			live_display = { "VirtualText" },
+			live_mode_toggle = "enable",
+		})
 	end,
 }
