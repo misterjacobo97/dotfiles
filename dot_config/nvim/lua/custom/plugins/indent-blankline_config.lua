9dc78d2f6c96d883897babd1b5cedb5c139ac956
jacobo <mister.jacobo97@gmail.com>
2024-05-03 00:52:26 +1000

    Update .config/nvim/init.lua Update .config/nvim/lua/custom/plugins/indent-blankline_config.lua Update .config/nvim/lua/custom/plugins/telescope_config.lua

--- dot_config/nvim/lua/custom/plugins/indent-blankline_config.lua
+++ dot_config/nvim/lua/custom/plugins/indent-blankline_config.lua
@@ -5,7 +5,6 @@ return {
 	config = function()
 		local whitespaceHighlights = {
 			"CursorColumn",
-			"Whitespace",
 		}
 
 		local indentHighlights = {
@@ -21,13 +20,13 @@ return {
 		local hooks = require("ibl.hooks")
 
 		hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-			vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-			vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-			vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-			vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-			vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-			vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-			vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+			vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#602C30" })
+			vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#84704A" })
+			vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#315675" })
+			vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#755639" })
+			vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#536B43" })
+			vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#5F4A6B" })
+			vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#29585E" })
 		end)
 
 		require("ibl").setup({
--- dot_config/nvim/lua/custom/plugins/telescope_config.lua
+++ dot_config/nvim/lua/custom/plugins/telescope_config.lua
@@ -2,10 +2,12 @@
 local telescope = require("telescope")
 
 telescope.setup({
-	-- ... your telescope config
+-- ... your telescope config
 })
 
 telescope.load_extension("chezmoi")
 vim.keymap.set("n", "<leader>cz", telescope.extensions.chezmoi.find_files, {})
+
+vim.keymap.set("n", '<leader>tl', require("telescope.builtin").live_grep, {})
 
 return {}
