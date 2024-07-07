10e021baa01ea4cd9cfd722138432f213df89665
jacobo <mister.jacobo97@gmail.com>
2024-07-07 21:19:08 +1000

    Update .chezmoidata/packages.yaml Add .config/nvim/lua/custom/plugins/venv-selector_config.lua

---
+++ dot_config/nvim/lua/custom/plugins/venv-selector_config.lua
@@ -0,0 +1,19 @@
+-- for use with python virtual environments - lets us switch venvs without restarting nvim
+
+return {
+	"linux-cultist/venv-selector.nvim",
+	dependencies = {
+		"neovim/nvim-lspconfig",
+		"mfussenegger/nvim-dap",
+		"mfussenegger/nvim-dap-python", --optional
+		{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+	},
+	lazy = false,
+	branch = "regexp", -- This is the regexp branch, use this for the new version
+	config = function()
+		require("venv-selector").setup()
+	end,
+	keys = {
+		{ ",v", "<cmd>VenvSelect<cr>" },
+	},
+}
